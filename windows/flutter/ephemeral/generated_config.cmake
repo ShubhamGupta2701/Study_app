e994f992759f38fe2d4ab98820833ef70f298146
ShubhamGupta <92706349+ShubhamGupta2701@users.noreply.github.com>
2023-02-19 22:25:50 +0530

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\sg628\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "D:\\study_app" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\sg628\\flutter"
+  "PROJECT_DIR=D:\\study_app"
+  "FLUTTER_ROOT=C:\\Users\\sg628\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=D:\\study_app\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=D:\\study_app"
+  "FLUTTER_TARGET=D:\\study_app\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=D:\\study_app\\.dart_tool\\package_config.json"
+)
